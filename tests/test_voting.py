19974c728d0ac7f2d343288ae989d316303881f0
Alex Parsons <alex.parsons@mysociety.org>
2023-11-19 21:18:24 +0000

    Move import into function for testing
    - Better independence between tests

--- tests/test_voting.py
+++ tests/test_voting.py
@@ -1,8 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
-from twfy_votes.apps.policies.vr_validator import test_policy_sample
 
 
 @pytest.mark.asyncio
 async def test_voting_records(client: TestClient):
+    from twfy_votes.apps.policies.vr_validator import test_policy_sample
+
     await test_policy_sample(5)
