a5f6624c561fe8e76bd3a87afc9e039a799d1a02
Alex Parsons <alex.parsons@mysociety.org>
2023-11-19 21:18:24 +0000

    Move import into function for testing
    - Better independence between tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
-from twfy_votes.main import app
 
 
 @pytest.fixture(scope="session")
 def client():
+    from twfy_votes.main import app
+
     with TestClient(app) as client:
         yield client
